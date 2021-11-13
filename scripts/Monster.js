8a7d73674fecbf436ede81f2b453fc1e104d3bf5
Bruno Zhong <69879040+Brunozhon@users.noreply.github.com>
2021-11-13 13:12:11 -0500

    Update Monster.js

--- scripts/Monster.js
+++ scripts/Monster.js
@@ -4,4 +4,15 @@ class Monster {
     this.hp = hp
     this.max_hp = max_hp
   }
+  takeDamage(damage) {
+    this.hp = this.hp - damage
+  }
+  get isDead() {
+    if (this.hp <= 0) {
+      return true
+    } else {
+      return false
+    }
+  }
 }
+export Monster;
