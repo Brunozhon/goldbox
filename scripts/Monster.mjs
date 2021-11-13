4ca43bb00c849d7eac6752ceb148f324bfd5a5e9
Bruno Zhong <69879040+Brunozhon@users.noreply.github.com>
2021-11-13 13:14:56 -0500

    Add semicolons and rename file

--- scripts/Monster.mjs
+++ scripts/Monster.mjs
@@ -1,17 +1,17 @@
 class Monster {
   constructor(name, hp, max_hp) {
-    this.name = name
-    this.hp = hp
-    this.max_hp = max_hp
+    this.name = name;
+    this.hp = hp;
+    this.max_hp = max_hp;
   }
   takeDamage(damage) {
-    this.hp = this.hp - damage
+    this.hp = this.hp - damage;
   }
   get isDead() {
     if (this.hp <= 0) {
-      return true
+      return true;
     } else {
-      return false
+      return false;
     }
   }
 }
